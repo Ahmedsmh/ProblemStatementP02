25959f78e43f4a85ef064d3407ad7fb244aef2f4
Javier <19045784@myrp.edu.sg>
2021-04-29 12:19:07 +0800

    InfoActivity updated

--- app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/InfoActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/InfoActivity.java
@@ -3,12 +3,30 @@ package sg.edu.rp.c346.id19008424.problemstatementp02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
+    ListView lv;
+    ArrayList<DailyGrade> alDG;
+    ArrayAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        lv = (ListView) findViewById(R.id.ListViewGrades);
+        alDG = new ArrayList<>();
+        alDG.add(new DailyGrade("B"));
+        alDG.add(new DailyGrade("C"));
+        alDG.add(new DailyGrade("A"));
+
+        aa = new GradeAdapter(this, R.layout.row, alDG);
+        lv.setAdapter(aa);
+
+
     }
 }
