08d0514d6365e60e84b4f2a9866d1d80436a285e
Ahmed <19008424@myrp.edu.sg>
2021-04-29 13:14:36 +0800

    Buttons

--- app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/InfoActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/InfoActivity.java
@@ -2,8 +2,12 @@ package sg.edu.rp.c346.id19008424.problemstatementp02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +16,7 @@ public class InfoActivity extends AppCompatActivity {
     ListView lv;
     ArrayList<DailyGrade> alDG;
     ArrayAdapter aa;
+    Button btnEmail, btnAdd, btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,45 @@ public class InfoActivity extends AppCompatActivity {
 
         aa = new GradeAdapter(this, R.layout.row, alDG);
         lv.setAdapter(aa);
+        btnEmail = findViewById(R.id.buttonEmail);
+        btnInfo = findViewById(R.id.buttonInfo);
+        btnAdd = findViewById(R.id.buttonAdd);
 
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                String grade = "";
+                for (int i = 0; i < alDG.size(); i++){
+                    grade += "Week " + (i+1) +": DG:" + alDG.get(i).getGrade() + "\n";
+                }
+                email.putExtra(Intent.EXTRA_TEXT,"Hi faci, \n I am... \n Please see my remarks so far, thank you! \n" + grade);
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+            }
+        });
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
 
     }
