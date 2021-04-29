a9dc19c0e7ae4b4e74ccaecf8aec7da89b095655
19036308 <19036308@myrp.edu.sg>
2021-04-29 11:45:06 +0800

    Complete Activity 1

--- app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/MainActivity.java
@@ -2,13 +2,41 @@ package sg.edu.rp.c346.id19008424.problemstatementp02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvModule;
+    ArrayAdapter aa;
+    ArrayList<String> modules;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvModule = (ListView) findViewById(R.id.listViewModule);
+        modules = new ArrayList<String>();
+        modules.add("C347");
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, modules);
+        lvModule.setAdapter(aa);
+
+        lvModule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent =  new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("holiday", modules.get(position));
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/sg/edu/rp/c346/id19008424/problemstatementp02/Module.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19008424.problemstatementp02;
+
+public class Module {
+
+    private String DG;
+
+    public Module(String DG) {
+        this.DG = DG;
+    }
+
+    public String getDG() {
+        return DG;
+    }
+}
